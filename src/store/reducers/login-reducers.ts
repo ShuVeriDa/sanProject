e5a5767174891860437099159c81ca6592ec01bc
ShuVeriDA <bashtarov@outlook.com>
2022-07-29 16:04:17 +0300

    created loginreducer.ts

--- src/store/reducers/login-reducers.ts
+++ src/store/reducers/login-reducers.ts
@@ -1,5 +1,39 @@
-export const loginReducers = (state: any, action: any) => {
-   switch (action.type) {
+import {authAPI, LoginRequestDataType} from "../../api/cards-api";
+import {AppThunk} from "../store";
+import {handleServerNetworkError} from "../../utils/error-utils";
 
+const AUTH_SET_AUTH_STATUS = '"AUTH/SET-AUTH-STATUS"'
+
+const initialState: initialStateType = {
+   isLoggedIn: false
+}
+
+export const loginReducers = (state: initialStateType = initialState, action: LoginActionsType) => {
+   switch (action.type) {
+      case AUTH_SET_AUTH_STATUS:
+         return {...state, isLoggedIn: action.isLoggedIn}
    }
 }
+
+//AC
+const setIsLoggedInAC = (isLoggedIn: boolean) =>
+   ({type: AUTH_SET_AUTH_STATUS, isLoggedIn} as const)
+
+//thunk
+export const loginTC = (data: LoginRequestDataType):AppThunk => (dispatch) => {
+   authAPI.login(data)
+      .then(res => {
+         setIsLoggedInAC(true)
+      })
+      .catch(error => {
+         handleServerNetworkError(error, dispatch)
+      })
+}
+
+//type
+type initialStateType = {
+   isLoggedIn: boolean
+}
+
+type SetIsLoggedInACType = ReturnType<typeof setIsLoggedInAC>
+export type LoginActionsType = SetIsLoggedInACType
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import {AnyAction, applyMiddleware, combineReducers, createStore} from "redux";
 import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {RegisterActionsType, registrationReducer} from "./reducers/registration-reducer";
-import {loginReducers} from "./reducers/login-reducers";
+import {LoginActionsType, loginReducers} from "./reducers/login-reducers";
 import {appReducer, AppReducerActionsType} from "../app/app-reducer";
 import {profileReducers} from "./reducers/profile-reducers";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
@@ -13,14 +13,14 @@ const rootReducer = combineReducers({
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
 
+//
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+export const useAppDispatch = () => useDispatch<DispatchType>()
+
 //type
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootActionsType>
 export type DispatchType = ThunkDispatch<AppRootStateType, unknown, AnyAction>
 export type AppRootActionsType = RegisterActionsType
    | AppReducerActionsType
-
-
-//
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
-export const useAppDispatch = () => useDispatch<DispatchType>()
+   | LoginActionsType
