09dfacbe64f4a0fd29940309da5986349dd85da3
ShuVeriDA <bashtarov@outlook.com>
2022-07-29 14:29:16 +0300

    added cards-api.ts

---
+++ src/api/cards-api.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+export const instance = axios.create({
+   baseURL: "https://neko-back.herokuapp.com/2.0/" || 'http://localhost:7542/2.0/',
+   withCredentials: true,
+})
+
+export const authAPI = {
+   register: (data: RegisterRequestDataType) => {
+      return instance.post('auth/register', data)
+   },
+   login: (data: LoginRequestDataType) => {
+      return instance.post('auth/login', data)
+   },
+   forgotPassword: (data: ForgotPassRequestDataType) => {
+      return instance.post('/auth/forgot', data)
+   }
+}
+
+//type
+
+export type RegisterRequestDataType = {
+   email: string
+   password: string
+}
+
+export type LoginRequestDataType = {
+   email: string
+   password: string
+   rememberMe: boolean
+}
+
+export type ForgotPassRequestDataType = {
+   email: string
+   from: string
+   message: string
+}
